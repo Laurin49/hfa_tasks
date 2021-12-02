8fad8e97f3ceaa47874c447dc6de457f742bb233
Laurin49 <welter@diwe.info>
2021-12-02 11:02:22 +0100

    DiffUtil zu Recyclerview hinzugefügt, Changes o.k.

--- app/src/main/java/info/diwe/tasks/TaskItemAdapter.kt
+++ app/src/main/java/info/diwe/tasks/TaskItemAdapter.kt
@@ -5,22 +5,15 @@ import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class TaskItemAdapter: RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>() {
-    var data = listOf<Task>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    override fun getItemCount(): Int {
-        return data.size
-    }
+// TaskItemAdapter uses TaskDiffItemCallback to compare its old data with the new.
+class TaskItemAdapter: ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
 
     // gets called when data needs to be displayed in a view holder
     override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
         holder.bind(item)
     }
 
